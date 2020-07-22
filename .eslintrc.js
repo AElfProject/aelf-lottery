bbc9280995828625dd48d2526600dae976ee4900
aelf-hz <mason.huang@aelf.io>
2020-07-22 19:18:38 +0800

    test: 💍 test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: './web/ReactNativeLottery/node_modules/@react-native-community',
+  extends: 'web/ReactNativeLottery/node_modules/@react-native-community/eslint-config',
 };
